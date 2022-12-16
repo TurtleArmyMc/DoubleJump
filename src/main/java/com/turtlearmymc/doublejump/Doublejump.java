7e4116c221b72fa29d96ffb7ef6dcfaf1f0c491f
TurtleArmyMc <44322335+TurtleArmyMc@users.noreply.github.com>
2022-12-16 17:43:16 -0500

    chore: Update to 1.19.3

--- src/main/java/com/turtlearmymc/doublejump/Doublejump.java
+++ src/main/java/com/turtlearmymc/doublejump/Doublejump.java
@@ -1,5 +1,7 @@
 package com.turtlearmymc.doublejump;
 
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,7 +14,6 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class Doublejump implements ModInitializer {
     public static Logger LOGGER = LogManager.getLogger();
@@ -28,7 +29,7 @@ public class Doublejump implements ModInitializer {
     @Override
     public void onInitialize() {
         log(Level.INFO, "Initializing");
-        DOUBLE_JUMP = Registry.register(Registry.ENCHANTMENT, new Identifier(MOD_ID, "doublejump"),
+        DOUBLE_JUMP = Registry.register(Registries.ENCHANTMENT, new Identifier(MOD_ID, "doublejump"),
                 new EnchantmentDoublejump(Enchantment.Rarity.RARE, new EquipmentSlot[] { EquipmentSlot.FEET }));
 
         ServerPlayNetworking.registerGlobalReceiver(C2S_DO_DOUBLEJUMP,
